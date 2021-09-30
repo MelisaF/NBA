19888a44207f58aeef7bb6708056268a3317dc0a
Agustin Artemisi <agustinartemisi@MacBook-Air-de-Agustin.local>
2021-09-30 12:21:21 -0300

    clone repository

--- src/components/Card/CardProductos.jsx
+++ src/components/Card/CardProductos.jsx
@@ -1,3 +1,21 @@
 import {Card} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import '../estilos.css';
+
+
+export const CardProductos = ({img, id, precio, nombre, modelo}) => {
+    return (
+        <Card border="0" style={{ width: '18rem'}}>
+            <Card.Img variant="top" src={img} alt={modelo}/>
+            <Card.Body>
+                <Card.Title>
+                    Id: {id} <br/>
+                    Precio: ${precio} <br/>
+                    Nombre: {nombre}<br/>
+                    Modelo: {modelo}<br/>
+                </Card.Title>
+                <Link to={`/producto/${id}`}className="btn btn-dark btn-compra">VER DETALLE</Link>
+            </Card.Body>
+        </Card>
+    )
+}
